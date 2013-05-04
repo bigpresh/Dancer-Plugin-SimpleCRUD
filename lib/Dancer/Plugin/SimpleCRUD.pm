92bf3b8f08f0b2b621da97f610a51968805ec221
David Precious <davidp@preshweb.co.uk>
2013-05-04 19:14:00 +0100

    Add acknowledgement for Rahul - sorry it's late!
    Looks like I forgot to add an acknowledgement for Rahul Kotamaraju last time
    round - sorry Rahul!

--- lib/Dancer/Plugin/SimpleCRUD.pm
+++ lib/Dancer/Plugin/SimpleCRUD.pm
@@ -36,7 +36,7 @@ use Dancer::Plugin::Database;
 use HTML::Table::FromDatabase;
 use CGI::FormBuilder;
 
-our $VERSION = '0.80';
+our $VERSION = '0.90';
 
 =encoding utf8
 
@@ -1299,6 +1299,8 @@ saberworks
 jasonjayr
 
 Paul Johnson (pjcj)
+
+Rahul Kotamaraju
 
 Michael J South (msouth)
 
